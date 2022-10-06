a0a6026d07a7d414a5a3dbe5a0ff0dcf6c2eced3
user1 <user1@DESKTOP-F9KAO7M>
2022-10-06 15:11:51 +0900

    실습

---
+++ java_demo02/src/kr/co/exam/Exam01.java
@@ -0,0 +1,36 @@
+package kr.co.exam;
+
+public class Exam01 {
+
+	public static void main(String[] args) {
+//		1. 문자열 변수 name에 자신의 이름 저장
+		
+//		2. 정수 변수 age에 자신의 나이 저장
+		
+//		3. 실수 변수 tall에 자신의 키 저장
+		
+//		4. 변수에 저장된 모든 값을 .println() 함수로 출력하세요.
+		
+//		5. 변수에 저장된 값을 활용하여 다음 형식에 맞추어 출력하세요.
+//		   형식에 맞추어 출력할 때 .print() / .printf() / .println() 중
+//		   자신이 사용하기 편한 함수 하나를 선택하여 출력하세요.
+//		   형식 : 저의 이름은 홍길동 입니다. 올해 30세 이며, 내년에는 31세가 됩니다.
+		
+		
+		String name = "서채현";
+		int age = 27;
+		double tall = 163.0;
+		
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(tall);
+		
+//		.print()
+		System.out.print("저의 이름은 " + name + " 입니다. 올해 " + age + "세 이며, 내년에는 " + (age+1) + "세가 됩니다.\n");
+//		.printf()
+		System.out.printf("저의 이름은 %s 입니다. 올해 %d세 이며, 내년에는 %d세가 됩니다.\n", name, age, (age+1));
+//		.println()
+		System.out.println("저의 이름은 " + name + " 입니다. 올해 " + age + "세 이며, 내년에는 " + (age+1) + "세가 됩니다.");
+	}
+
+}
