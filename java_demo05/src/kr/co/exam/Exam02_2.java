fd3033a3727dba516ec71ea966cbaec4593ab739
silkstring <why612@naver.com>
2022-10-13 10:02:58 +0900

    배열 연습문제 풀이

---
+++ java_demo05/src/kr/co/exam/Exam02_2.java
@@ -0,0 +1,31 @@
+package kr.co.exam;
+
+import java.util.Scanner;
+
+public class Exam02_2 {
+
+	public static void main(String[] args) {
+//		Exam02.java 파일을 생성 후 다음의 문제를 풀어보세요.
+//		사용자 입력으로 5~10 사이의 정수 값을 입력받아
+//		입력받은 정수값과 동일한 크기의 배열을 생성하세요.
+//		그리고 배열의 값은 -1로 초기화하세요.
+		
+		Scanner sc = new Scanner(System.in);
+		int size = 0;
+		
+		while(!(size >= 5 && size <= 10)) {
+			System.out.println("5 ~ 10 사이의 정수를 입력하세요.");
+			size = sc.nextInt();
+		}
+		
+		int arr1[] = new int[size];
+		
+		for(int i = 0; i < size; i++) {
+			arr1[i] = -1;
+		}
+		for(int k = 0; k < arr1.length; k++) {
+			System.out.print(arr1[k] + "\t");
+		}
+	}
+
+}
