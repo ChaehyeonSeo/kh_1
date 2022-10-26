d2f4b6b5c31637add823e6fedb16bfee20586642
silkstring <why612@naver.com>
2022-10-26 15:08:15 +0900

    StringBuilder

--- java_demo08/src/kr/co/demo02/Demo.java
+++ java_demo08/src/kr/co/demo02/Demo.java
@@ -15,14 +15,45 @@ public class Demo {
 		 * 					   공유 자원에 대한 안정성을 높여주기 위한 기능
 		 */
 		
+//		append() : 문자열 추가
 		StringBuilder sb = new StringBuilder();
 		sb.append("문자열");
 		sb.append("추가");
-		System.out.println(sb);
+		System.out.println(sb.toString());
 		
-		String str = "문자열";
-		str += "추가";
-		System.out.println(str);
+//		capacity() : 버퍼크기
+		int capacity = sb.capacity();
+		System.out.println(capacity);
+		
+//		delete() : 문자열 삭제
+		sb = sb.delete(0, 3);
+		System.out.println(sb.toString());
+		
+//		insert() : 지정한 위치에 문자열 추가
+		sb = sb.insert(0, "문자열");
+		System.out.println(sb.toString());
+		sb = sb.insert(3, " insert() ");
+		System.out.println(sb.toString());
+		
+//		length() : 문자열 길이
+		int length = sb.length();
+		System.out.println(length);
+		
+//		reverse() : 문자열을 역순으로 나열
+		sb.reverse();
+		System.out.println(sb.toString());
+		
+//		setLength() : 문자열 길이 변경
+		sb.setLength(20);
+		sb.reverse();
+		System.out.println(sb.toString());
+
+//		trimToSize() : 버퍼크기를 문자열 길이로 변경
+		sb.trimToSize();
+		System.out.println(sb.capacity());
+		
+//		sb.indexOf(), sb.replace(), sb.charAt(), sb.compareTo(), sb.substring()
+//		위 메서드는 기존 String 클래스에서 사용한 메서드와 동일한 기능 수행
 	}
 	
 }
