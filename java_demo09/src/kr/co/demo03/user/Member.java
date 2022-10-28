e26b5d35bf431199b343ebdb5c7612064d9a72ad
silkstring <why612@naver.com>
2022-10-28 15:13:20 +0900

    예외 및 사용자 정의 예외

---
+++ java_demo09/src/kr/co/demo03/user/Member.java
@@ -0,0 +1,26 @@
+package kr.co.demo03.user;
+
+import kr.co.demo03.user.exception.UserException;
+
+public class Member {
+	
+	private String name;
+	
+	public Member() {}
+	
+	public Member(String name) throws UserException {
+		this.setName(name);
+	}
+	
+	public void setName(String name) throws UserException{
+		if (name != null) {
+			this.name = name;
+		} else {
+			throw new UserException("잘못된 사용자 이름");
+		}
+	}
+
+	public String getName() {
+		return this.name;
+	}
+}
