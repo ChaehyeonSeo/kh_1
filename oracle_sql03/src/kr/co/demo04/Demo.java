96d2bcfb30c716ed6fa65e3c8e9195742afa8369
silkstring <why612@naver.com>
2022-11-18 11:05:57 +0900

    VO 객체를 생성하여 적용

--- oracle_sql03/src/kr/co/demo04/Demo.java
+++ oracle_sql03/src/kr/co/demo04/Demo.java
@@ -1,7 +1,7 @@
 package kr.co.demo04;
 
 import java.sql.Connection;
-import java.sql.Date;
+import java.util.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -10,38 +10,48 @@ import java.sql.Statement;
 import java.text.SimpleDateFormat;
 
 import kr.co.db.connection.OracleConnection;
+import kr.co.db.vo.EmployeeVO;
 
 public class Demo {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		OracleConnection oc = new OracleConnection("localhost:1521/XEPDB1", "dev01");
 		
-		int empId = 207;
-		String jobId = "IT_PROG";
-		Date hireDate = new Date(new java.util.Date().getTime());
+		EmployeeVO emp = new EmployeeVO();
+		emp.setEmpId(207);
+		emp.setFirstName("길동");
+		emp.setLastName("홍");
+		emp.setEmail("HGILDONG");
+		emp.setPhoneNumber("515.123.1234");
+		emp.setHireDate(new Date());
+		emp.setJobId("IT_PROG");
+		emp.setSalary(2800);
+		emp.setCommission(0);
+		emp.setManagerId(103);
+		emp.setDeptId(60);
 		
 		String query = "INSERT INTO EMPLOYEES VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 		
 		// Statement 또는 PreparedStatement 객체 생성
 		PreparedStatement pstat = oc.getPrepared(query);
-		pstat.setInt(1, empId);
-		pstat.setString(2, "길동");
-		pstat.setString(3, "홍");
-		pstat.setString(4, "HGILDONG");
-		pstat.setString(5, "515.123.1234");
-		pstat.setDate(6, hireDate);
-		pstat.setString(7, jobId);
-		pstat.setInt(8, 2800);
-		pstat.setDouble(9, 0);
-		pstat.setInt(10, 103);
-		pstat.setInt(11, 60);
+		pstat.setInt(1, emp.getEmpId());
+		pstat.setString(2, emp.getFirstName());
+		pstat.setString(3, emp.getLastName());
+		pstat.setString(4, emp.getEmail());
+		pstat.setString(5, emp.getPhoneNumber());
+		pstat.setDate(6, emp.getHireDate());
+		pstat.setString(7, emp.getJobId());
+		pstat.setInt(8, emp.getSalary());
+		pstat.setDouble(9, emp.getCommission());
+		pstat.setInt(10, emp.getManagerId());
+		pstat.setInt(11, emp.getDeptId());
 		
 		int rs = oc.sendInsert();
 		
 		if(rs >= 1) {
 			System.out.println(rs + "개 행이 반영되었습니다.");
 		} else {
-			System.out.println("0개 행이 반영되었습니다. (쿼리에 문제가 있는 것 같습니다. 다시 확인하세요.)");
+			System.out.println("0개 행이 반영되었습니다. (쿼리에 문제가 있는것 같습니다. 다시 확인하세요.)");
 		}
 		
 		oc.close();
