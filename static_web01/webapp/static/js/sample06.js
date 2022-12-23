f7219c75d96e489842e4002ffe3081e9fff0c772
silkstring <why612@naver.com>
2022-12-23 14:43:29 +0900

    자바스크립트 엘리먼트 추가/삭제

--- static_web01/webapp/static/js/sample06.js
+++ static_web01/webapp/static/js/sample06.js
@@ -10,8 +10,8 @@
  *      형제.after(추가할엘리먼트) : 뒤로 추가
  */
 
+/* 목록을 추가하는 함수 */
 function addItem(form) {
-    /* 목록을 추가하는 함수 */
     var item = form.item.value;
     var list = document.getElementById("list");
 
@@ -27,14 +27,14 @@ function addItem(form) {
     form.item.value = "";
 }
 
+/* 
+    테이블에 행을 추가하는 함수(5열) 
+    제목/작성자 : 사용자 입력
+    번호 : tbody의 행 수 + 1,
+    작성일 : new Date() 객체 사용하여 현재 날짜
+    조회수 : 기본값 0
+*/
 function addRow(form) {
-    /* 
-        테이블에 행을 추가하는 함수(5열) 
-        제목/작성자 : 사용자 입력
-        번호 : tbody의 행 수 + 1,
-        작성일 : new Date() 객체 사용하여 현재 날짜
-        조회수 : 기본값 0
-    */
     var tbody = document.getElementById("table").lastElementChild;
     var today = new Date();
     var year = today.getFullYear();
@@ -63,4 +63,38 @@ function createTableRow(columnCount) {
         row.append(column);
     }
     return row;
+}
+
+function validCheck(form) {
+    /**
+     * 동일한 패스워드를 사용하는지 검사 후 동일하지 않은 값을 입력한 경우
+     * 패스워드 확인 입력 양식 옆에 "다시 입력하시오." 메시지 출력
+     * username에서는 6자리 이상의 아이디가 아닌 경우 
+     * 아이디 입력 양식 옆에 "6자리 이상 입력하시오." 메시지 출력
+     */
+    var isValid = true;
+    if(form.username.nextElementSibling !== null) {
+        form.username.nextElementSibling.remove();
+    }
+    if(form.username.value.length < 6) {
+        spanErrorMessage(form.username, "6자리 이상 입력하시오.");
+        form.username.value = "";
+        isValid = false;
+    }
+    if(form.passwordCheck.nextElementSibling !== null) {
+        form.passwordCheck.nextElementSibling.remove();
+    }
+    if(form.password.value !== form.passwordCheck.value) {
+        spanErrorMessage(form.passwordCheck, "다시 입력하시오.");
+        form.passwordCheck.value = "";
+        isValid = false;
+    }
+    return isValid;
+}
+
+function spanErrorMessage(element, message) {
+    let span = document.createElement("span");
+    span.innerText = message;
+    element.after(span);
+    return span;
 }
